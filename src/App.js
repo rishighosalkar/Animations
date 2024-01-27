e253c2b5bd124a745b02f72b87a5b339381a0a69
Rushikesh Ghosalkar <rushighosalkar@gmail.com>
2024-01-28 01:16:12 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import tenor from './assets/tenor.gif';
 function App() {
+  const [left, setLeft] = useState();
+  const [top, setTop] = useState();
+
+  const handleHover = () => {
+    console.log('hover');
+    // const t1= Math.floor(Math.random()*500)+1;
+    // const t2 = Math.floor(Math.random()*500)+1;
+    // const i = t1 < window.innerWidth ? t1: (window.innerWidth - (window.innerWidth*0.9));
+    // const j = t2 < window.innerHeight ? t2: (window.innerHeight - (window.innerHeight*0.9));
+    const i = Math.floor(Math.random()*(window.innerWidth - 100));
+    const j = Math.floor(Math.random()*(window.innerHeight - 40));
+    console.log(i+" "+j);
+    setLeft(i);
+    setTop(j);
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h2>Do you wanna go out with me?</h2>
+        <img src={tenor} alt='' className='App-logo'/>
+        <div className='btn-group'>
+          <button className='btn-yes'>Yes</button>
+          <button className='btn-no'
+            onMouseOver={handleHover}
+            onTouchStart={handleHover}
+            style={{
+               left: `${left}px`, top: `${top}px`
+            }}
+          >No</button>
+      </div>
+      
     </div>
   );
 }
